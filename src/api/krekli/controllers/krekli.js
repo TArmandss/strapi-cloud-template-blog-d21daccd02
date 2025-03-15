b3b358b737e8cf59888e464c81599222f3e4a891
TArmandss <armandstaskanss@gmail.com>
2025-03-15 13:16:36 +0200

    1st commit

--- src/api/krekli/controllers/krekli.js
+++ src/api/krekli/controllers/krekli.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * krekli controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::krekli.krekli');
